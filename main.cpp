92901bf641a55108a7fe718c2f83ea1cf5c918ed
Johnny-droid <joaoricardoralves@gmail.com>
2022-01-18 22:08:26 +0000

    Inital Commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World of Graphs!" << std::endl;
+    std::cout << "We love you Graphs!" << std::endl;
+    std::cout << "(Well, we need to)" << std::endl;
+    return 0;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using testing::Eq;
+
+TEST(something, likeSomething) {
+    EXPECT_EQ(1, 1);
+}
